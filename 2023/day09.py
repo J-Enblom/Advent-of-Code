dd1c9afbc4a41dd8e9511cf84d74929b32e8c4a9
John Enblom <enblom2@gmail.com>
2023-12-09 07:02:54 +0100

    Day 9 cleanup

--- 2023/day09.py
+++ 2023/day09.py
@@ -18,36 +18,24 @@ def rec_finder(seq):
         differences.append(seq[i+1]- seq[i])
         i += 1
     if all(v == 0 for v in differences):
-        return seq[-1]
-    dif = rec_finder(differences)
-    return seq[-1] + dif
+        return seq[-1], seq[0]
+    dif_l, dif_f = rec_finder(differences)
+    last = seq[-1] + dif_l
+    first = seq[0] - dif_f
+    return last, first
 
-def part1(data):
+def solve(data):
     input = parse(data)
-    total = 0
+    total_last = 0
+    total_first = 0
     for seq in input:
-        total += rec_finder(seq)
-    return total
-
-def rec_finder2(seq):
-    i = 0
-    differences = []
-    while i < len(seq) - 1:
-        differences.append(seq[i+1]- seq[i])
-        i += 1
-    if all(v == 0 for v in differences):
-        return seq[0]
-    dif = rec_finder2(differences)
-    return seq[0] - dif
-
-def part2(data):
-    input = parse(data)
-    total = 0
-    for seq in input:
-        total += rec_finder2(seq)
-    return total
+        last, first = rec_finder(seq)
+        total_last += last
+        total_first += first
+    return total_last, total_first
 
 if __name__ == "__main__":
     data = input()
-    print("part 1:", part1(data))
-    print("part 2:", part2(data))
+    part1, part2 = solve(data)
+    print("part 1:", part1)
+    print("part 2:", part2)
